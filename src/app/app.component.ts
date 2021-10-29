82a68f4c83459448724bd40ce73bfe4d4c80d9b4
muzaffer <aydogdum38@gmail.com>
2021-10-29 11:28:39 +0300

    chart js angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,73 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild, ElementRef, OnInit} from '@angular/core';
+import 'chartjs-plugin-annotation';
+
+// declare const $: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'chartTest';
+
+
+export class AppComponent  implements  OnInit {
+
+  @ViewChild('myCanvas')
+  public canvas: ElementRef;
+  public context: CanvasRenderingContext2D;
+  public chartType = 'line';
+  public chartData: any[];
+  public chartLabels: any[];
+  public chartColors: any[];
+  public chartOptions: any;
+
+  ngOnInit() {
+
+    this.chartData = [{
+      data: [3, 1, 4, 2, 5],
+      label: 'Anthracnose',
+      fill: false
+    }];
+    this.chartLabels = ['Jan', 'Feb', 'Mar', 'Apr', 'May'];
+    this.chartColors = [{
+      backgroundColor: 'rgba(0, 0, 0, 0.2)',
+      borderColor: 'rgba(0, 0, 0, 1)'
+    }];
+    this.chartOptions = {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true,
+            stepSize: 1
+          }
+        }]
+      },
+      annotation: {
+        drawTime: 'beforeDatasetsDraw',
+        annotations: [{
+          type: 'box',
+          id: 'a-box-1',
+          yScaleID: 'y-axis-0',
+          yMin: 0,
+          yMax: 1,
+          backgroundColor: 'gray'
+        }, {
+          type: 'box',
+          id: 'a-box-2',
+          yScaleID: 'y-axis-0',
+          yMin: 1,
+          yMax: 2.7,
+          backgroundColor: 'yellow'
+        }, {
+          type: 'box',
+          id: 'a-box-3',
+          yScaleID: 'y-axis-0',
+          yMin: 2.7,
+          yMax: 5,
+          backgroundColor: 'gray'
+        }]
+      }
+    };
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule} from '@angular/core';
 import { AppComponent } from './app.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { ChartsModule } from 'ng2-charts';
+import { BarChartComponent } from './bar-chart/bar-chart.component';
+import { PieChartComponent } from './pie-chart/pie-chart.component';
+import {RouterModule, Routes} from '@angular/router';
+// import {NgApexchartsModule} from 'ng-apexcharts';
+
+
+const routes: Routes = [
+  {path: 'bar-chart', component: BarChartComponent},
+  {path: 'pie-chart', component: PieChartComponent},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BarChartComponent,
+    PieChartComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule.withServerTransition({appId: 'serverApp'}),
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(routes),
+    ChartsModule,
+    // NgApexchartsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
